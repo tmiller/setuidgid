f9aa1e58a47afffe055160f34efabfa38222a3c0
Tom Miller <jackerran@gmail.com>
2016-06-15 22:27:01 -0500

    Initial prototype
    Influenced by the following programs:
    
    * daemontools by Daniel J. Bernstein
            - setuidgid
    * s6 Project by Laurent Bercot
            - s6-setuidgid
            - s6-applyuidgid

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+func main() {
+
+	//path := os.Getenv("PATH")
+
+	pw, err := user.Lookup(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	uid, err := strconv.Atoi(pw.Uid)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	gid, err := strconv.Atoi(pw.Gid)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// fmt.Println(pw.Uid)
+	syscall.Setuid(uid)
+	syscall.Setgid(gid)
+	if strings.HasPrefix(os.Args[2], "/") {
+		err := syscall.Exec(os.Args[2], os.Args[2:], os.Environ())
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
